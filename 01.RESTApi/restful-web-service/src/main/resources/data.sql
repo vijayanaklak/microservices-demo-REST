e30c4874e1607b8a2aaf53ab4b8917d3a41dd63a
Vijayanak L <iamvijayanak@gmail.com>
2020-09-28 12:53:04 +0530

    removed unwanted files

--- 01.RESTApi/restful-web-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user values(10001,sysdate(),'Vijay')
-insert into user values(10002,sysdate(),'Irin')
-insert into user values(10003,sysdate(),'Hanna')
-insert into post values(11001,'This is first Post', 10001)
-insert into post values(11002,'This is second Post', 10001)
-insert into post values(11003,'This is third Post', 10001)
